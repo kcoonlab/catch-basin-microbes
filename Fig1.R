fcddf4f332c539c1b0757851db66256aadc482d9
Kerri L. Coon <kerri.coon@wisc.edu>
2024-10-17 17:31:05 -0500

    Update Fig1.R

--- Fig1.R
+++ Fig1.R
@@ -1,19 +1,24 @@
 library("ggmap")
 
+## Fig 1A. Map of study jurisdiction
+
 sites <- read.table("metadata.txt", sep = "\t", header = TRUE)
 sites <- subset(sites,sample_control=="sample")
-ll_means <- sapply(sites[91:92], mean, na.rm=TRUE) #point to lat and lon columns. may be different col number with different iterations of metadata file
-map_og <- get_map(location = ll_means,  maptype = "satellite", source = "google", zoom = 15) 
-
+ll_means <- sapply(sites[91:92], mean, na.rm=TRUE) #point to lat and lon columns
 map_region <- get_map(location = ll_means,  maptype = "terrain", source = "google", zoom = 6) 
 region_point <- ggmap(map_region) + 
-  geom_point(data = subset(sites,UCB=="41"),
+  geom_point(data = subset(sites, UCB=="41"),
              color = "red", size = 6, shape=8, alpha=0.7)+
   labs(x="Longitude", y="Latitude")
 region_point
 
-cook <- read.table("WQ_Dips_2021.csv",sep=",",header=TRUE)
+## Fig 1B. Distribution of basins by basin type and flow group
 
+sites <- read.table("metadata.txt", sep = "\t", header = TRUE)
+sites <- subset(sites,sample_control=="sample")
+ll_means <- sapply(sites[91:92], mean, na.rm=TRUE) #point to lat and lon columns
+map_og <- get_map(location = ll_means,  maptype = "satellite", source = "google", zoom = 15) 
+cook <- read.table("WQ_Dips_2021.csv",sep=",",header=TRUE)
 basins_flowgroup <- ggmap(map_og) + 
   geom_jitter(data = subset(cook, Date=="2021-06-11"), #Date=="6/11/21"
              mapping = aes(x=lon, y=lat, color= Flowgroup_coarse, shape=combined_separate),
