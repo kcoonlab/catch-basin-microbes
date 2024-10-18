5906583aebed369614ef16de42475862b067181f
Kerri L. Coon <kerri.coon@wisc.edu>
2024-10-18 11:58:12 -0500

    Add files via upload

---
+++ 5.phyloseqobject.R
@@ -0,0 +1,15 @@
+
+# creating a phyloseq object from the qiime outputs
+
+library(phyloseq)
+library(ggplot2)
+library(qiime2R)
+
+ps.all <- qza_to_phyloseq("table_analysis.qza", 
+                           "mafft-fasttree-output/rooted_tree.qza", 
+                           "taxass_taxonomy_98.qza", 
+                           "metadata.txt")
+ps.all
+
+# will create a new phyloseq object using a rerooted tree (see table 2 script)
+# this will be used for all downstream analyses
