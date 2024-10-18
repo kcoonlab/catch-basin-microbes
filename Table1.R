ce5fc47849a320e2bc8e7a982698872e5165fcde
Kerri L. Coon <kerri.coon@wisc.edu>
2024-10-18 13:51:46 -0500

    Update Table1.R

--- Table1.R
+++ Table1.R
@@ -1,93 +1,147 @@
-## Table 1 Linear regressions
+## Table 1. Effects of water quality on mosquito productivity
 
-## Table 1A data aggregated by basin
+library(dplyr)
+library(nlme)
 
-cook.basins.2021 <- read.csv("summary_basins_2021.csv",sep=",", header=TRUE)
-summary(lm(Pupae.prev ~ pH.avg, data = cook.basins.2021))
-summary(lm(Pupae.prev ~ Temp.C.avg, data = cook.basins.2021))
-summary(lm(Pupae.prev ~ Cond.avg, data = cook.basins.2021))
-summary(lm(Pupae.prev ~ DO.avg, data = cook.basins.2021))
-summary(lm(Pupae.prev ~ Sal.avg, data = cook.basins.2021))
+## Table 1A. Linear regression analyses with data aggregated by basin
 
-summary(lm(Pupae.avg ~ pH.avg, data = cook.basins.2021))
-summary(lm(Pupae.avg ~ Temp.C.avg, data = cook.basins.2021))
-summary(lm(Pupae.avg ~ Cond.avg, data = cook.basins.2021))
-summary(lm(Pupae.avg ~ DO.avg, data = cook.basins.2021))
-summary(lm(Pupae.avg ~ Sal.avg, data = cook.basins.2021))
+WQ.data <- read.csv("input-files/WQ_Dips_2021_Final.csv",sep=",", header=TRUE)
+WQ.data["rain"][cook["rain"]=="trace"] <- 0.001
+WQ.data$rain <- as.numeric(cook$rain)
+WQ.data.by.basins <- WQ.data %>%
+  group_by(UCB) %>%
+  dplyr::summarize(Pupae.avg = mean(Pupae, na.rm=TRUE),
+            Pupae.sd = sd(Pupae, na.rm=TRUE),
+            Pupae.min = min(Pupae, na.rm=TRUE),
+            Pupae.max = max(Pupae, na.rm=TRUE),
+            Pupae.prev = sum(Pupae>=1, na.rm=TRUE)/sum(Pupae>=0, na.rm=TRUE),
+            Fail.prop = sum(Fail==1, na.rm=TRUE)/sum(Fail>=0,na.rm=TRUE),            
+            pH.avg = mean(pH, na.rm=TRUE),
+            pH.sd = sd(pH, na.rm=TRUE),
+            pH.min = min(pH, na.rm=TRUE),
+            pH.max = max(pH, na.rm=TRUE),
+            Temp.C.avg = mean(Temp.C, na.rm=TRUE),
+            Temp.C.sd = sd(Temp.C, na.rm=TRUE),
+            Temp.C.min = min(Temp.C, na.rm=TRUE),
+            Temp.C.max = max(Temp.C, na.rm=TRUE),
+            DO.avg = mean(DO, na.rm=TRUE),
+            DO.sd = sd(DO, na.rm=TRUE),
+            DO.min = min(DO, na.rm=TRUE),
+            DO.max = max(DO, na.rm=TRUE),
+            Sal.avg = mean(Sal, na.rm=TRUE),
+            Sal.sd = sd(Sal, na.rm=TRUE),
+            Sal.min = min(Sal, na.rm=TRUE),
+            Sal.max = max(Sal, na.rm=TRUE),
+            Cond.avg = mean(Cond, na.rm=TRUE),
+            Cond.sd = sd(Cond, na.rm=TRUE),
+            Cond.min = min(Cond, na.rm=TRUE),
+            Cond.max = max(Cond, na.rm=TRUE)
+            )
 
-***## Table 1B data aggregated by sampling date
+summary(lm(Pupae.prev ~ pH.avg, data = WQ.data.by.basins))
+summary(lm(Pupae.prev ~ Temp.C.avg, data = WQ.data.by.basins))
+summary(lm(Pupae.prev ~ Cond.avg, data = WQ.data.by.basins))
+summary(lm(Pupae.prev ~ DO.avg, data = WQ.data.by.basins))
+summary(lm(Pupae.prev ~ Sal.avg, data = WQ.data.by.basins))
 
-cook <- read.table("WQ_Dips_2020_2021.csv",sep=",",header=TRUE)
-cook["rain"][cook["rain"]=="trace"] <- 0.0001
-cook$rain <- as.numeric(cook$rain)
-cook <- subset(cook, Year=="2021")
-cook$Date<- factor(cook$Date, levels = c("4/15/21","6/4/21","6/11/21","6/18/21","6/25/21","7/2/21","7/9/21","7/16/21","7/23/21","7/30/21","8/6/21","8/13/21","8/20/21","8/27/21","9/3/21","9/10/21","9/17/21","9/24/21"))
-cook$rain %>% class
-cook["rain"][cook["rain"]=="trace"] <- 0.001
-cook$rain <- as.numeric(cook$rain)
+summary(lm(Pupae.avg ~ pH.avg, data = WQ.data.by.basins))
+summary(lm(Pupae.avg ~ Temp.C.avg, data = WQ.data.by.basins))
+summary(lm(Pupae.avg ~ Cond.avg, data = WQ.data.by.basins))
+summary(lm(Pupae.avg ~ DO.avg, data = WQ.data.by.basins))
+summary(lm(Pupae.avg ~ Sal.avg, data = WQ.data.by.basins))
 
-cook.day <- cook %>% 
+## Table 1B. Linear regression analyses with data aggregated by sampling date
+
+WQ.data.by.date <- WQ.data %>%
   group_by(Date) %>%
-  dplyr::summarize(rainfall = mean(rain, na.rm=TRUE),
-                  Pupae.avg = mean(Pupae, na.rm=TRUE),
-                   Pupae.total = sum(Pupae, na.rm=TRUE),
-                  Pupae.prev = sum(Pupae>=1, na.rm=TRUE)/sum(Pupae>=0, na.rm=TRUE),
-                   Moz.avg = mean(Moz.total, na.rm=TRUE),
-                   Moz.total = sum(Moz.total, na.rm=TRUE),
-                   Moz.prev = sum(Moz.total>=1, na.rm=TRUE)/sum(Moz.total>=0, na.rm=TRUE),
-                  Fail.prop = sum(Fail>=1, na.rm=TRUE)/sum(Fail>=0, na.rm=TRUE),
-                  pH.avg = mean(pH, na.rm=TRUE),
-                  Temp.avg = mean(Temp.C, na.rm=TRUE),
-                  Cond.avg = mean(Cond, na.rm=TRUE),
-                  DO.avg = mean(DO, na.rm=TRUE),
-                  Sal.avg = mean(Sal, na.rm=TRUE)
-                  )
+  dplyr::summarize(Pupae.avg = mean(Pupae, na.rm=TRUE),
+            Pupae.sd = sd(Pupae, na.rm=TRUE),
+            Pupae.min = min(Pupae, na.rm=TRUE),
+            Pupae.max = max(Pupae, na.rm=TRUE),
+            Pupae.prev = sum(Pupae>=1, na.rm=TRUE)/sum(Pupae>=0, na.rm=TRUE),
+            Fail.prop = sum(Fail==1, na.rm=TRUE)/sum(Fail>=0,na.rm=TRUE),            
+            pH.avg = mean(pH, na.rm=TRUE),
+            pH.sd = sd(pH, na.rm=TRUE),
+            pH.min = min(pH, na.rm=TRUE),
+            pH.max = max(pH, na.rm=TRUE),
+            Temp.C.avg = mean(Temp.C, na.rm=TRUE),
+            Temp.C.sd = sd(Temp.C, na.rm=TRUE),
+            Temp.C.min = min(Temp.C, na.rm=TRUE),
+            Temp.C.max = max(Temp.C, na.rm=TRUE),
+            DO.avg = mean(DO, na.rm=TRUE),
+            DO.sd = sd(DO, na.rm=TRUE),
+            DO.min = min(DO, na.rm=TRUE),
+            DO.max = max(DO, na.rm=TRUE),
+            Sal.avg = mean(Sal, na.rm=TRUE),
+            Sal.sd = sd(Sal, na.rm=TRUE),
+            Sal.min = min(Sal, na.rm=TRUE),
+            Sal.max = max(Sal, na.rm=TRUE),
+            Cond.avg = mean(Cond, na.rm=TRUE),
+            Cond.sd = sd(Cond, na.rm=TRUE),
+            Cond.min = min(Cond, na.rm=TRUE),
+            Cond.max = max(Cond, na.rm=TRUE)
+            )
+WQ.data.by.date[sapply(WQ.data.by.date, is.nan)] <- NA
+WQ.data.by.date[sapply(WQ.data.by.date, is.infinite)] <- NA
 
-summary(lm(Pupae.prev ~ pH.avg, data = cook.day))
-summary(lm(Pupae.prev ~ Temp.C.avg, data = cook.day))
-summary(lm(Pupae.prev ~ Cond.avg, data = cook.day))
-summary(lm(Pupae.prev ~ DO.avg, data = cook.day))
-summary(lm(Pupae.prev ~ Sal.avg, data = cook.day))
+summary(lm(Pupae.prev ~ pH.avg, data = WQ.data.by.date))
+summary(lm(Pupae.prev ~ Temp.C.avg, data = WQ.data.by.date))
+summary(lm(Pupae.prev ~ Cond.avg, data = WQ.data.by.date))
+summary(lm(Pupae.prev ~ DO.avg, data = WQ.data.by.date))
+summary(lm(Pupae.prev ~ Sal.avg, data = WQ.data.by.date))
 
-summary(lm(Pupae.avg ~ pH.avg, data = cook.day))
-summary(lm(Pupae.avg ~ Temp.C.avg, data = cook.day))
-summary(lm(Pupae.avg ~ Cond.avg, data = cook.day))
-summary(lm(Pupae.avg ~ DO.avg, data = cook.day))
-summary(lm(Pupae.avg ~ Sal.avg, data = cook.day))
+summary(lm(Pupae.avg ~ pH.avg, data = WQ.data.by.date))
+summary(lm(Pupae.avg ~ Temp.C.avg, data = WQ.data.by.date))
+summary(lm(Pupae.avg ~ Cond.avg, data = WQ.data.by.date))
+summary(lm(Pupae.avg ~ DO.avg, data = WQ.data.by.date))
+summary(lm(Pupae.avg ~ Sal.avg, data = WQ.data.by.date))
 
+## Table 1C. Linear mixed-effects models with unaggregated data (observations only where pupae were present)
 
-## Table 1C Linear mixed-effects models with unaggregated data (observations only where pupae were present)
+WQ.data.pupaepresent <- subset(WQ.data.pupaepresent, Pupae > "0")
+WQ.data.pupaepresent$Datefactor <- WQ.data.pupaepresent$Date %>% as.factor
 
-cook <- read.table("WQ_Dips_2020_2021.csv",sep=",",header=TRUE)
-cook <- subset(cook, Year=="2021")
-cook <- subset(cook, Date > "2021-04-16")
-cook.pupaepresent <- subset(cook, Pupae > "0")
-cook.pupaepresent <- subset(cook.pupaepresent, !is.na(pH)==TRUE)
-cook.pupaepresent$Datefactor <- cook.pupaepresent$Date %>% as.factor
-cook.pupaepresent = cook.pupaepresent %>%
+WQ.data.pupaepresent.pH <- subset(WQ.data.pupaepresent, !is.na(pH)==TRUE)
+WQ.data.pupaepresent.pH = WQ.data.pupaepresent.pH %>%
      arrange(UCB, Datefactor)                     
 model = lme(sqrt(Pupae) ~ pH,
               random = ~1|UCB, 
               correlation = corAR1(), 
-              data = cook.pupaepresent)
+              data = WQ.data.pupaepresent.pH)
 summary(model)
+
+WQ.data.pupaepresent.Temp.C <- subset(WQ.data.pupaepresent, !is.na(Temp.C)==TRUE)
+WQ.data.pupaepresent.Temp.C = WQ.data.pupaepresent.Temp.C %>%
+     arrange(UCB, Datefactor)      
 model = lme(sqrt(Pupae) ~ Temp.C,
               random = ~1|UCB, 
               correlation = corAR1(), 
-              data = cook.pupaepresent)
+              data = WQ.data.pupaepresent.Temp.C)
 summary(model)
+
+WQ.data.pupaepresent.Cond <- subset(WQ.data.pupaepresent, !is.na(Cond)==TRUE)
+WQ.data.pupaepresent.Cond = WQ.data.pupaepresent.Cond %>%
+     arrange(UCB, Datefactor) 
 model = lme(sqrt(Pupae) ~ Cond,
               random = ~1|UCB, 
               correlation = corAR1(), 
-              data = cook.pupaepresent)
+              data = WQ.data.pupaepresent.Cond)
 summary(model)
+
+WQ.data.pupaepresent.DO <- subset(WQ.data.pupaepresent, !is.na(DO)==TRUE)
+WQ.data.pupaepresent.DO = WQ.data.pupaepresent.DO %>%
+     arrange(UCB, Datefactor)
 model = lme(sqrt(Pupae) ~ DO,
               random = ~1|UCB, 
               correlation = corAR1(), 
-              data = cook.pupaepresent)
+              data = WQ.data.pupaepresent.DO)
 summary(model)
+
+WQ.data.pupaepresent.Sal <- subset(WQ.data.pupaepresent, !is.na(Sal)==TRUE)
+WQ.data.pupaepresent.Sal = WQ.data.pupaepresent.Sal %>%
+     arrange(UCB, Datefactor)
 model = lme(sqrt(Pupae) ~ Sal,
               random = ~1|UCB, 
               correlation = corAR1(), 
-              data = cook.pupaepresent)
+              data = WQ.data.pupaepresent.Sal)
 summary(model)
