c14530957f647a738bb272497a1ef93bb1a93e3b
Kerri L. Coon <kerri.coon@wisc.edu>
2024-10-18 12:13:45 -0500

    Update Fig1.R

--- Fig1.R
+++ Fig1.R
@@ -1,8 +1,10 @@
-library("ggmap")
+## Fig 1. Locations of collection sites
+
+library(ggmap)
 
 ## Fig 1A. Map of study jurisdiction
 
-sites <- read.table("metadata.txt", sep = "\t", header = TRUE)
+sites <- read.table("input-files/metadata.txt", sep = "\t", header = TRUE)
 sites <- subset(sites,sample_control=="sample")
 ll_means <- sapply(sites[91:92], mean, na.rm=TRUE) #point to lat and lon columns
 map_region <- get_map(location = ll_means,  maptype = "terrain", source = "google", zoom = 6) 
@@ -14,11 +16,8 @@ region_point
 
 ## Fig 1B. Distribution of basins by basin type and flow group
 
-sites <- read.table("metadata.txt", sep = "\t", header = TRUE)
-sites <- subset(sites,sample_control=="sample")
-ll_means <- sapply(sites[91:92], mean, na.rm=TRUE) #point to lat and lon columns
 map_og <- get_map(location = ll_means,  maptype = "satellite", source = "google", zoom = 15) 
-cook <- read.table("WQ_Dips_2021.csv",sep=",",header=TRUE)
+cook <- read.table("input-files/WQ_Dips_2021.csv",sep=",",header=TRUE)
 basins_flowgroup <- ggmap(map_og) + 
   geom_jitter(data = subset(cook, Date=="2021-06-11"), #Date=="6/11/21"
              mapping = aes(x=lon, y=lat, color= Flowgroup_coarse, shape=combined_separate),
