21d4dbf363f4e4b078e465823a88dcf117b8c47c
Kerri L. Coon <kerri.coon@wisc.edu>
2024-10-20 12:38:36 -0500

    Update Fig5.R

--- Fig5.R
+++ Fig5.R
@@ -64,10 +64,10 @@ C39.pupaepa <- ggplot(data=subset(metadata,Pupae_pa != "NA"),
 C39.pupaepa
 kruskal.test(C39 ~ Pupae_pa, data = metadata)
 
-***## Fig 5E. Pupae per dip by relative abundance of C39
+## Fig 5E. Pupae per dip by relative abundance of C39
 
-ggplot(metadata.naUCBrm.allCBs, aes(x=C39, y=sqrt(Pupae), color=date_code))+
-  geom_point()
+ggplot(metadata, aes(x=C39, y=sqrt(Pupae))) +
+  geom_point(color = "black", fill = "black")
   
 ## Fig 5F. Relative abundance of C39 strains over time
 
@@ -101,24 +101,6 @@ metadata.add <- metadata.add %>% rownames_to_column(var="sampleid")
 metadata.relabund <- join_all(list(relabund.C39.asvs, metadata.add), by='sampleid',type='left')
 metadata.relabund <- subset(metadata.relabund, metadata.relabund$sampleid != "asvid")
 
-                                      
-#metadata.relabund$`1c896ad60b318aea6eb67595fe68d2bf` <- metadata.relabund$`X1c896ad60b318aea6eb67595fe68d2bf` %>% as.numeric
-#metadata.relabund$`56f81ede4d385001b8135ac61458befe` <- metadata.relabund$`X56f81ede4d385001b8135ac61458befe` %>% as.numeric
-#metadata.relabund$`713852f86aa38fb1f6f5291dee0db276` <- metadata.relabund$`X713852f86aa38fb1f6f5291dee0db276` %>% as.numeric
-#metadata.relabund$`3813a6b872a29e05f211916bfb3397ef` <- metadata.relabund$`X3813a6b872a29e05f211916bfb3397ef` %>% as.numeric
-#metadata.relabund$`00a1a1e7419bd374f7b8993902555db6` <- metadata.relabund$`X00a1a1e7419bd374f7b8993902555db6` %>% as.numeric
-#metadata.relabund$`34e720bc46aa554b53222a0915c6410e` <- metadata.relabund$`X34e720bc46aa554b53222a0915c6410e` %>% as.numeric
-#metadata.relabund$`37087046f6f5e933bf0dd42420f81cfe` <- metadata.relabund$`X37087046f6f5e933bf0dd42420f81cfe` %>% as.numeric
-#metadata.relabund$`912fefd634a7c68b38d929afd0f1cb68` <- metadata.relabund$`X912fefd634a7c68b38d929afd0f1cb68` %>% as.numeric
-#metadata.relabund$`9ae8011e9347ce18b60ba481bfb72e32` <- metadata.relabund$`X9ae8011e9347ce18b60ba481bfb72e32` %>% as.numeric
-#metadata.relabund$`a5f2920be80527d6ec323de39babdf2e` <- metadata.relabund$`a5f2920be80527d6ec323de39babdf2e` %>% as.numeric
-#metadata.relabund$`5ecd2e447fe9cfbef560d1c5e0738bb1` <- metadata.relabund$`X5ecd2e447fe9cfbef560d1c5e0738bb1` %>% as.numeric
-#metadata.relabund$`00a1a1e7419bd374f7b8993902555db6` %>% class
-
-#metadata.relabund$date <- factor(metadata.relabund$date, levels = c("4/15/21","6/11/21","6/25/21","7/9/21","7/23/21","8/6/21","8/27/21","9/17/21"))
-
-
-
 metadata.relabund.long <- reshape2::melt(data=metadata.relabund,
                                  id.vars=c("UCB","date"),
                                  measure.vars=c("37087046f6f5e933bf0dd42420f81cfe","34e720bc46aa554b53222a0915c6410e",
@@ -130,7 +112,10 @@ metadata.relabund.long <- reshape2::melt(data=metadata.relabund,
                                  variable.name="asvid",
                                  value.name="relabund") 
 
-c39_date <- ggplot(metadata.relabund.long, aes(x=date, y=relabund, color=asvid)) +
+metadata.relabund.long$date <- factor(metadata.relabund.long$date, levels = c("4/15/21","6/11/21","6/25/21","7/9/21","7/23/21","8/6/21","8/27/21","9/17/21"))
+metadata.relabund.long$relabund <- as.numeric(metadata.relabund.long$relabund)                               
+
+c39_date <- ggplot(metadata.relabund.long, aes(x=date, y=relabund, color=factor(asvid))) +
   geom_boxplot() +
   theme(axis.title = element_text(), 
         panel.grid.major = element_blank(), panel.grid.minor = element_blank(),
@@ -140,7 +125,3 @@ c39_date <- ggplot(metadata.relabund.long, aes(x=date, y=relabund, color=asvid))
                    labels=c("4/15","6/11","6/25","7/9","7/23","8/6","8/27","9/17")) +
   ylab("Relative abundance")+ xlab(NULL)+ labs(color="ASV ID")
 c39_date
-
-
-
-
