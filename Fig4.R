500e999967f117bf2037425731308897a44bcc71
Kerri L. Coon <kerri.coon@wisc.edu>
2024-10-21 18:57:23 -0500

    Update Fig4.R

--- Fig4.R
+++ Fig4.R
@@ -9,20 +9,20 @@ library(gplots)
 # Identifying key taxa (Pupal occurrence)
 
 set.seed(123)
-ps.all <- readRDS("input-files/ps.all.rds")
-ps.all.phylum <- tax_glom(ps.all, "Phylum", NArm = TRUE)
-ps.all.phylum.pupaepresence <- subset_samples(ps.all.phylum, !(Pupae_pa =="NA"))
-sample_data(ps.all.phylum.pupaepresence)$Pupae_pa <- as.factor(sample_data(ps.all.phylum.pupaepresence)$Pupae_pa)
+ps.final <- readRDS("input-files/ps.final.rds")
+ps.final.phylum <- tax_glom(ps.final, "Phylum", NArm = TRUE)
+ps.final.phylum.pupaepresence <- subset_samples(ps.final.phylum, !(Pupae.pres =="NA"))
+sample_data(ps.final.phylum.pupaepresence)$Pupae.pres <- as.factor(sample_data(ps.final.phylum.pupaepresence)$Pupae.pres)
 
-aldex.results <- ALDEx2::aldex(data.frame(phyloseq::otu_table(ps.all.phylum.pupaepresence)), 
-                               phyloseq::sample_data(ps.all.phylum.pupaepresence)$Pupae_pa,
+aldex.results <- ALDEx2::aldex(data.frame(phyloseq::otu_table(ps.final.phylum.pupaepresence)), 
+                               phyloseq::sample_data(ps.final.phylum.pupaepresence)$Pupae.pres,
                                    test="t", effect = TRUE, denom="iqlr")
 
 aldex.results.sig <- aldex.results %>% rownames_to_column(var = "taxon") %>% filter(wi.eBH < 0.05) %>% arrange(effect, wi.eBH) %>%
   dplyr::select(taxon, diff.btw, diff.win, effect, wi.ep, wi.eBH)
 aldex.results.effectsize <- aldex.results %>% rownames_to_column(var = "taxon") %>% arrange(wi.eBH, effect) %>%
   dplyr::select(taxon, diff.btw, diff.win, effect, wi.ep, wi.eBH)
-taxa_info <- data.frame(tax_table(ps.all.phylum.pupaepresence))
+taxa_info <- data.frame(tax_table(ps.final.phylum.pupaepresence))
 taxa_info <- taxa_info %>% rownames_to_column(var = "taxon")
 aldex.results.effectsize.tax <- left_join(aldex.results.effectsize, taxa_info)
 aldex.results.sig.tax.pupaepa <- left_join(aldex.results.sig, taxa_info)
@@ -30,33 +30,33 @@ aldex.results.sig.tax.pupaepa
 
 # Identifying key taxa (Pupal abundance)
 
-ps.all.phylum.pupae <- subset_samples(ps.all.phylum, !(Pupae =="NA"))
+ps.final.phylum.pupae <- subset_samples(ps.final.phylum, !(Pupae.abund =="NA"))
 
-aldex.cont <- aldex.clr(as.data.frame(otu_table(ps.all.phylum.pupae)),
-                        sample_data(ps.all.phylum.pupae)$Pupae,
+aldex.cont <- aldex.clr(as.data.frame(otu_table(ps.final.phylum.pupae)),
+                        sample_data(ps.final.phylum.pupae)$Pupae.abund,
                         mc.samples=128,
-                        denom="all",
+                        denom="final",
                         verbose=TRUE,
                         useMC=FALSE)
-aldex.cont.results <- aldex.corr(aldex.cont, sample_data(ps.all.phylum.pupae)$Pupae)
+aldex.cont.results <- aldex.corr(aldex.cont, sample_data(ps.final.phylum.pupae)$Pupae.abund)
 
 aldex.cont.sig <- aldex.cont.results %>% rownames_to_column(var = "taxon") %>% filter(pearson.ep < 0.05) %>%
   arrange(pearson.ep, pearson.ecor) %>% dplyr::select(taxon, pearson.ecor, pearson.ep, pearson.eBH)
-taxa_info <- data.frame(tax_table(ps.all.phylum.pupae))
+taxa_info <- data.frame(tax_table(ps.final.phylum.pupae))
 taxa_info <- taxa_info %>% rownames_to_column(var = "taxon")
 aldex.cont.taxa.pupae <- left_join(aldex.cont.sig, taxa_info)
 aldex.cont.taxa.pupae
 
 # Identifying key taxa (Basin type = Separated)
 
-aldex.results <- ALDEx2::aldex(data.frame(phyloseq::otu_table(ps.all.phylum)), 
-                               phyloseq::sample_data(ps.all.phylum)$combined_separate,
+aldex.results <- ALDEx2::aldex(data.frame(phyloseq::otu_table(ps.final.phylum)), 
+                               phyloseq::sample_data(ps.final.phylum)$Basin.type,
                                    test="t", effect = TRUE, denom="iqlr")
 aldex.results.sig <- aldex.results %>% rownames_to_column(var = "taxon") %>% filter(wi.eBH < 0.05) %>% arrange(effect, wi.eBH) %>%
   dplyr::select(taxon, diff.btw, diff.win, effect, wi.ep, wi.eBH)
 aldex.results.effectsize <- aldex.results %>% rownames_to_column(var = "taxon") %>% arrange(wi.eBH, effect) %>%
   dplyr::select(taxon, diff.btw, diff.win, effect, wi.ep, wi.eBH)
-taxa_info <- data.frame(tax_table(ps.all.phylum))
+taxa_info <- data.frame(tax_table(ps.final.phylum))
 taxa_info <- taxa_info %>% rownames_to_column(var = "taxon")
 aldex.results.effectsize.tax <- left_join(aldex.results.effectsize, taxa_info)
 aldex.results.sig.tax.basintype <- left_join(aldex.results.sig, taxa_info)
@@ -64,76 +64,76 @@ aldex.results.sig.tax.basintype
 
 # Identifying key taxa (pH)
 
-ps.all.phylum.pH <- subset_samples(ps.all.phylum, !(pH =="NA"))
+ps.final.phylum.pH <- subset_samples(ps.final.phylum, !(pH =="NA"))
 
-aldex.cont <- aldex.clr(as.data.frame(otu_table(ps.all.phylum.pH)),
-                        sample_data(ps.all.phylum.pH)$pH,
+aldex.cont <- aldex.clr(as.data.frame(otu_table(ps.final.phylum.pH)),
+                        sample_data(ps.final.phylum.pH)$pH,
                         mc.samples=128,
-                        denom="all",
+                        denom="final",
                         verbose=TRUE,
                         useMC=FALSE)
-aldex.cont.results <- aldex.corr(aldex.cont, sample_data(ps.all.phylum.pH)$pH)
+aldex.cont.results <- aldex.corr(aldex.cont, sample_data(ps.final.phylum.pH)$pH)
 
 aldex.cont.sig <- aldex.cont.results %>% rownames_to_column(var = "taxon") %>% filter(pearson.ep < 0.05) %>%
   arrange(pearson.ep, pearson.ecor) %>% dplyr::select(taxon, pearson.ecor, pearson.ep, pearson.eBH)
-taxa_info <- data.frame(tax_table(ps.all.phylum.pH))
+taxa_info <- data.frame(tax_table(ps.final.phylum.pH))
 taxa_info <- taxa_info %>% rownames_to_column(var = "taxon")
 aldex.cont.taxa.pH <- left_join(aldex.cont.sig, taxa_info)
 aldex.cont.taxa.pH
 
 # Identifying key taxa (DO)
 
-ps.all.phylum.DO <- subset_samples(ps.all.phylum, !(DO =="NA"))
+ps.final.phylum.DO <- subset_samples(ps.final.phylum, !(DO =="NA"))
 
-aldex.cont <- aldex.clr(as.data.frame(otu_table(ps.all.phylum.DO)),
-                        sample_data(ps.all.phylum.DO)$DO,
+aldex.cont <- aldex.clr(as.data.frame(otu_table(ps.final.phylum.DO)),
+                        sample_data(ps.final.phylum.DO)$DO,
                         mc.samples=128,
-                        denom="all",
+                        denom="final",
                         verbose=TRUE,
                         useMC=FALSE)
-aldex.cont.results <- aldex.corr(aldex.cont, sample_data(ps.all.phylum.DO)$DO)
+aldex.cont.results <- aldex.corr(aldex.cont, sample_data(ps.final.phylum.DO)$DO)
 
 aldex.cont.sig <- aldex.cont.results %>% rownames_to_column(var = "taxon") %>% filter(pearson.ep < 0.05) %>%
   arrange(pearson.ep, pearson.ecor) %>% dplyr::select(taxon, pearson.ecor, pearson.ep, pearson.eBH)
-taxa_info <- data.frame(tax_table(ps.all.phylum.DO))
+taxa_info <- data.frame(tax_table(ps.final.phylum.DO))
 taxa_info <- taxa_info %>% rownames_to_column(var = "taxon")
 aldex.cont.taxa.DO <- left_join(aldex.cont.sig, taxa_info)
 aldex.cont.taxa.DO
 
 # Identifying key taxa (Cond)
 
-ps.all.phylum.Cond <- subset_samples(ps.all.phylum, !(Cond =="NA"))
+ps.final.phylum.Cond <- subset_samples(ps.final.phylum, !(Cond =="NA"))
 
-aldex.cont <- aldex.clr(as.data.frame(otu_table(ps.all.phylum.Cond)),
-                        sample_data(ps.all.phylum.Cond)$Cond,
+aldex.cont <- aldex.clr(as.data.frame(otu_table(ps.final.phylum.Cond)),
+                        sample_data(ps.final.phylum.Cond)$Cond,
                         mc.samples=128,
-                        denom="all",
+                        denom="final",
                         verbose=TRUE,
                         useMC=FALSE)
-aldex.cont.results <- aldex.corr(aldex.cont, sample_data(ps.all.phylum.Cond)$Cond)
+aldex.cont.results <- aldex.corr(aldex.cont, sample_data(ps.final.phylum.Cond)$Cond)
 
 aldex.cont.sig <- aldex.cont.results %>% rownames_to_column(var = "taxon") %>% filter(pearson.ep < 0.05) %>%
   arrange(pearson.ep, pearson.ecor) %>% dplyr::select(taxon, pearson.ecor, pearson.ep, pearson.eBH)
-taxa_info <- data.frame(tax_table(ps.all.phylum.Cond))
+taxa_info <- data.frame(tax_table(ps.final.phylum.Cond))
 taxa_info <- taxa_info %>% rownames_to_column(var = "taxon")
 aldex.cont.taxa.Cond <- left_join(aldex.cont.sig, taxa_info)
 aldex.cont.taxa.Cond
 
 # Identifying key taxa (Sal)
 
-ps.all.phylum.Sal <- subset_samples(ps.all.phylum, !(Sal =="NA"))
+ps.final.phylum.Sal <- subset_samples(ps.final.phylum, !(Sal =="NA"))
 
-aldex.cont <- aldex.clr(as.data.frame(otu_table(ps.all.phylum.Sal)),
-                        sample_data(ps.all.phylum.Sal)$Sal,
+aldex.cont <- aldex.clr(as.data.frame(otu_table(ps.final.phylum.Sal)),
+                        sample_data(ps.final.phylum.Sal)$Sal,
                         mc.samples=128,
-                        denom="all",
+                        denom="final",
                         verbose=TRUE,
                         useMC=FALSE)
-aldex.cont.results <- aldex.corr(aldex.cont, sample_data(ps.all.phylum.Sal)$Sal)
+aldex.cont.results <- aldex.corr(aldex.cont, sample_data(ps.final.phylum.Sal)$Sal)
 
 aldex.cont.sig <- aldex.cont.results %>% rownames_to_column(var = "taxon") %>% filter(pearson.ep < 0.05) %>%
   arrange(pearson.ep, pearson.ecor) %>% dplyr::select(taxon, pearson.ecor, pearson.ep, pearson.eBH)
-taxa_info <- data.frame(tax_table(ps.all.phylum.Sal))
+taxa_info <- data.frame(tax_table(ps.final.phylum.Sal))
 taxa_info <- taxa_info %>% rownames_to_column(var = "taxon")
 aldex.cont.taxa.Sal <- left_join(aldex.cont.sig, taxa_info)
 aldex.cont.taxa.Sal
@@ -173,7 +173,7 @@ aldex.cont.taxa.Sal <- aldex.cont.taxa.Sal[,-c(1,3,5,7:11)]
 colnames(aldex.cont.taxa.Sal)[1] = "Effect"
 colnames(aldex.cont.taxa.Sal)[2] = "P-value"
 
-phylum_heatmap <- rbind(aldex.results.sig.tax.pupaepa, aldex.cont.taxa.pupae, aldex.results.sig.tax.basintype, aldex.cont.taxa.pH, aldex.cont.taxa.Cond, aldex.cont.taxa.DO, aldex.cont.taxa.Sal)
+phylum_heatmap <- rbind(aldex.results.sig.tax.basintype, aldex.results.sig.tax.pupaepa, aldex.cont.taxa.pupae, aldex.cont.taxa.pH, aldex.cont.taxa.Cond, aldex.cont.taxa.DO, aldex.cont.taxa.Sal)
 phylum_heatmap <- phylum_heatmap[,-2]
 phylum_heatmap <- subset(phylum_heatmap, Phylum!="unclassified.Bacteria") 
 phylum_heatmap_wide <- reshape(data = phylum_heatmap,
