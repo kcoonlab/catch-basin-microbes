e85543917991ec6e47011527d301ca7810467d48
Kerri L. Coon <kerri.coon@wisc.edu>
2024-10-21 19:27:52 -0500

    Update Table2.R

--- Table2.R
+++ Table2.R
@@ -10,21 +10,21 @@ library(tidyr)
 ## Table 2A  PERMANOVA tests using PhILR distances between samples
 
 set.seed(123)
-ps.all <- readRDS("input-files/ps.all.rds")
-sample_data(ps.all)$sample.id <- sample_names(ps.all)
+ps.final <- readRDS("input-files/ps.final.rds")
+sample_data(ps.final)$sample.id <- sample_names(ps.final)
 
-# running with just individual NA samples cut (not all instances of a UCB with an NA anywhere)
-NA %in% sample_data(ps.all)$pH #row 39
-NA %in% sample_data(ps.all)$Temp # row 39,129
-NA %in% sample_data(ps.all)$DO # row 39
-NA %in% sample_data(ps.all)$Sal # row 39, 71
-NA %in% sample_data(ps.all)$Cond # row 39
-sample_data(ps.all)[39,] #sample.id = B_73
-sample_data(ps.all)[71,] #sample.id = C_61
-sample_data(ps.all)[129,] #sample.id = F_56
+# running with just individual NA samples cut (not final instances of a UCB with an NA anywhere)
+NA %in% sample_data(ps.final)$pH #row 39
+NA %in% sample_data(ps.final)$Temp.C # row 39,129
+NA %in% sample_data(ps.final)$DO # row 39
+NA %in% sample_data(ps.final)$Sal # row 39, 71
+NA %in% sample_data(ps.final)$Cond # row 39
+sample_data(ps.final)[39,] #sample.id = B_73
+sample_data(ps.final)[71,] #sample.id = C_61
+sample_data(ps.final)[129,] #sample.id = F_56
 
 # Take out the 3 individual samples that have NAs, and use that remaining set for the permanovas
-ps.most <- subset_samples(ps.all, !sample.id %in% c("B_73","C_61","F_56"))
+ps.most <- subset_samples(ps.final, !sample.id %in% c("B_73","C_61","F_56"))
 samples.most <- as.data.frame(as.matrix(sample_data(ps.most)))
 
 ps.most.pseudocount <- ps.most
@@ -34,52 +34,51 @@ dist.most.philr <- dist(ps.most.philr, method="euclidean")
 #saveRDS(dist.most.philr, "dist_most_philr_Table2.rds")
 
 perm_design <- how(within = Within(type = "series"),
-    plots = Plots(strata = samples.most$UCB),
+    plots = Plots(strata = samples.most$Basin.id),
     nperm = 999)
 
-adonis2(dist.most.philr ~ date, data=samples.most, permutations=perm_design)
+adonis2(dist.most.philr ~ Sampling.date, data=samples.most, permutations=perm_design)
 adonis2(dist.most.philr ~ pH, data=samples.most, permutations=perm_design)
-adonis2(dist.most.philr ~ Temp, data=samples.most, permutations=perm_design)
+adonis2(dist.most.philr ~ Temp.C, data=samples.most, permutations=perm_design)
 adonis2(dist.most.philr ~ DO, data=samples.most, permutations=perm_design)
-adonis2(dist.most.philr ~ Sal, data=samples.most, permutations=perm_design)
 adonis2(dist.most.philr ~ Cond, data=samples.most, permutations=perm_design)
+adonis2(dist.most.philr ~ Sal, data=samples.most, permutations=perm_design)
 
-permutest(betadisper(dist.most.philr, samples.most$date))
-permutest(betadisper(dist.most.philr, samples.most$Temp))
-permutest(betadisper(dist.most.philr, samples.most$Sal))
+permutest(betadisper(dist.most.philr, samples.most$Sampling.date))
+permutest(betadisper(dist.most.philr, samples.most$Temp.C))
 permutest(betadisper(dist.most.philr, samples.most$Cond))
+permutest(betadisper(dist.most.philr, samples.most$Sal))
 
-samples.all <- as.data.frame(as.matrix(sample_data(ps.all)))
-samples.pupaepresent <- samples.all %>% drop_na(Pupae_pa)
+samples.final <- as.data.frame(as.matrix(sample_data(ps.final)))
+samples.pupaepresent <- samples.final %>% drop_na(Pupae.pres)
 samples.pupaepresent <- samples.pupaepresent[-c(27,59,116),]
 m <- as.matrix(dist.most.philr) 
 dist.pupaepresent.philr <- as.dist(m[-c(1:12,121), -c(1:12,121)])
 
 perm_design <- how(within = Within(type = "series"),
-    plots = Plots(strata = samples.pupaepresent$UCB),
+    plots = Plots(strata = samples.pupaepresent$Basin.id),
     nperm = 999)
 
-adonis2(dist.pupaepresent.philr ~ Pupae_pa, data=samples.pupaepresent, permutations=perm_design)
-adonis2(dist.pupaepresent.philr ~ Pupae, data=samples.pupaepresent, permutations=perm_design)
+adonis2(dist.pupaepresent.philr ~ Pupae.pres, data=samples.pupaepresent, permutations=perm_design)
+adonis2(dist.pupaepresent.philr ~ Pupae.abund, data=samples.pupaepresent, permutations=perm_design)
 
-permutest(betadisper(dist.pupaepresent.philr, samples.pupaepresent$Pupae_pa))
-permutest(betadisper(dist.pupaepresent.philr, samples.pupaepresent$Pupae))
+permutest(betadisper(dist.pupaepresent.philr, samples.pupaepresent$Pupae.pres))
+permutest(betadisper(dist.pupaepresent.philr, samples.pupaepresent$Pupae.abund))
 
 ## Table 2B  PERMANOVA tests using PhILR distances between samples aggregated by basin
 
-ps.all.byUCB <- merge_samples(ps.all,"UCB")
-sample_data(ps.all.byUCB)$Flowgroup_coarse <- c("MinerEvanstonRammer","MinerEvanstonRammer","MinerEvanstonRammer","MinerEvanstonRammer","DonaldBanta","DonaldBanta","DonaldBanta","MinerEvanstonRammer","MinerEvanstonRammer","MinerEvanstonRammer","MinerEvanstonRammer","MinerEvanstonRammer","MinerEvanstonRammer","MinerEvanstonRammer","MinerEvanstonRammer","MinerEvanstonRammer","MinerEvanstonRammer","DonaldBanta","MinerEvanstonRammer","MinerEvanstonRammer","Stratford","Stratford","Stratford","Stratford","Stratford","Gibbons","Gibbons","Stratford","Stratford","Stratford","Stratford","Gibbons","Gibbons","MayfairCarlyle","MayfairCarlyle","MayfairCarlyle","MayfairCarlyle","MayfairCarlyle","MayfairCarlyle","MayfairCarlyle","MayfairCarlyle","MayfairCarlyle")
-sample_data(ps.all.byUCB)$combined_separate <- c("separate","separate","separate","separate","separate","separate","separate","separate","separate","separate","separate","separate","separate","separate","separate","separate","separate","separate","separate","separate","combined","combined","combined","combined","combined","combined","combined","combined","combined","combined","combined","combined","combined","combined","combined","combined","combined","combined","combined","combined","combined","combined")
-samples.all.byUCB <- as.data.frame(as.matrix(sample_data(ps.all.byUCB)))
+ps.final.byUCB <- merge_samples(ps.final,"Basin.id")
+sample_data(ps.final.byUCB)$Basin.flowgroup <- c("MinerEvanstonRammer","MinerEvanstonRammer","MinerEvanstonRammer","MinerEvanstonRammer","DonaldBanta","DonaldBanta","DonaldBanta","MinerEvanstonRammer","MinerEvanstonRammer","MinerEvanstonRammer","MinerEvanstonRammer","MinerEvanstonRammer","MinerEvanstonRammer","MinerEvanstonRammer","MinerEvanstonRammer","MinerEvanstonRammer","MinerEvanstonRammer","DonaldBanta","MinerEvanstonRammer","MinerEvanstonRammer","Stratford","Stratford","Stratford","Stratford","Stratford","Gibbons","Gibbons","Stratford","Stratford","Stratford","Stratford","Gibbons","Gibbons","MayfairCarlyle","MayfairCarlyle","MayfairCarlyle","MayfairCarlyle","MayfairCarlyle","MayfairCarlyle","MayfairCarlyle","MayfairCarlyle","MayfairCarlyle")
+sample_data(ps.final.byUCB)$Basin.type <- c("separate","separate","separate","separate","separate","separate","separate","separate","separate","separate","separate","separate","separate","separate","separate","separate","separate","separate","separate","separate","combined","combined","combined","combined","combined","combined","combined","combined","combined","combined","combined","combined","combined","combined","combined","combined","combined","combined","combined","combined","combined","combined")
+samples.final.byUCB <- as.data.frame(as.matrix(sample_data(ps.final.byUCB)))
 
-ps.all.byUCB.pseudocount <- ps.all.byUCB
-otu_table(ps.all.byUCB.pseudocount) <- t(otu_table(ps.all.byUCB) + 1)
-ps.all.byUCB.philr <- philr(ps.all.byUCB.pseudocount, part.weights='enorm.x.gm.counts', ilr.weights='blw.sqrt')
-dist.all.byUCB.philr <- dist(ps.all.byUCB.philr, method="euclidean")
-#saveRDS(dist.all.byUCB.philr, "dist_all_byUCB_philr_Table2.rds")
+ps.final.byUCB.pseudocount <- ps.final.byUCB
+otu_table(ps.final.byUCB.pseudocount) <- t(otu_table(ps.final.byUCB) + 1)
+ps.final.byUCB.philr <- philr(ps.final.byUCB.pseudocount, part.weights='enorm.x.gm.counts', ilr.weights='blw.sqrt')
+dist.final.byUCB.philr <- dist(ps.final.byUCB.philr, method="euclidean")
+#saveRDS(dist.final.byUCB.philr, "dist_final_byUCB_philr_Table2.rds")
 
-adonis2(dist.all.byUCB.philr ~ combined_separate, data=samples.all.byUCB)
-adonis2(dist.all.byUCB.philr ~ Flowgroup_coarse, data=samples.all.byUCB)
+adonis2(dist.final.byUCB.philr ~ Basin.type, data=samples.final.byUCB)
+adonis2(dist.final.byUCB.philr ~ Basin.flowgroup, data=samples.final.byUCB)
 
-permutest(betadisper(dist.all.byUCB.philr, samples.all.byUCB$combined_separate))
-permutest(betadisper(dist.all.byUCB.philr, samples.all.byUCB$Flowgroup_coarse))
+permutest(betadisper(dist.final.byUCB.philr, samples.final.byUCB$Basin.type))
