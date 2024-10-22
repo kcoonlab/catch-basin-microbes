c2424f6432e4cae650ab24167dc35b923264e8df
Kerri L. Coon <kerri.coon@wisc.edu>
2024-10-21 20:20:38 -0500

    Update Table3.R

--- Table3.R
+++ Table3.R
@@ -5,20 +5,20 @@ library(dplyr)
 metadata <- read.table("input-files/metadata.txt", sep="\t", header=TRUE)
 metadata <- subset(metadata, sample_control=="sample")
 metadata.byCB <- metadata %>% 
-  group_by(UCB) %>%
-  dplyr::summarize(Pupae.prev = sum(Pupae>=1, na.rm=TRUE)/sum(Pupae>=0, na.rm=TRUE),
-  				   Pupae.avg = mean(Pupae, na.rm=TRUE),
-  				   shannon_unrar.avg = mean(shannon_unrar, na.rm=TRUE),
-  				   richness.avg = mean(features_unrar, na.rm=TRUE),
-  				   C39.avg = mean(C39, na.rm=TRUE),
-  				   Proteo.avg = mean(Proteobacteria, na.rm=TRUE))
+  group_by(Basin.id) %>%
+  dplyr::summarize(Pupae.prev = sum(Pupae.pres>=1, na.rm=TRUE)/sum(Pupae.pres>=0, na.rm=TRUE),
+  				   Pupae.abund.avg = mean(Pupae.abund, na.rm=TRUE),
+  				   Shannon.avg = mean(Shannon, na.rm=TRUE),
+  				   ASV.richness.avg = mean(ASV.richness, na.rm=TRUE),
+  				   C39.relabund.avg = mean(C39.relabund, na.rm=TRUE),
+  				   Proteobacteria.relabund.avg = mean(Proteobacteria.relabund, na.rm=TRUE))
 
-summary(lm(Pupae.prev ~ shannon_unrar.avg, data = metadata.byCB))
-summary(lm(Pupae.prev ~ richness.avg, data = metadata.byCB))
-summary(lm(Pupae.prev ~ C39.avg, data = metadata.byCB))
-summary(lm(Pupae.prev ~ Proteo.avg, data = metadata.byCB))
+summary(lm(Pupae.prev ~ Shannon.avg, data = metadata.byCB))
+summary(lm(Pupae.prev ~ ASV.richness.avg, data = metadata.byCB))
+summary(lm(Pupae.prev ~ C39.relabund.avg, data = metadata.byCB))
+summary(lm(Pupae.prev ~ Proteobacteria.relabund.avg, data = metadata.byCB))
 
-summary(lm(Pupae.avg ~ shannon_unrar.avg, data = metadata.byCB))
-summary(lm(Pupae.avg ~ richness.avg, data = metadata.byCB))
-summary(lm(Pupae.avg ~ C39.avg, data = metadata.byCB))
-summary(lm(Pupae.avg ~ Proteo.avg, data = metadata.byCB))
+summary(lm(Pupae.abund.avg ~ Shannon.avg, data = metadata.byCB))
+summary(lm(Pupae.abund.avg ~ ASV.richness.avg, data = metadata.byCB))
+summary(lm(Pupae.abund.avg ~ C39.relabund.avg, data = metadata.byCB))
+summary(lm(Pupae.abund.avg ~ Proteobacteria.relabund.avg, data = metadata.byCB))
