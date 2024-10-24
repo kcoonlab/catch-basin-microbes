31d404464b1ec21f32ec341de275a1924663c623
Kerri L. Coon <kerri.coon@wisc.edu>
2024-10-24 13:40:46 -0500

    Update phyloseq-object.R

--- phyloseq-object.R
+++ phyloseq-object.R
@@ -21,22 +21,22 @@ pick_new_outgroup <- function(tree.unrooted){
   new.outgroup <- treeDT[which.max(length)]$id
   return(new.outgroup)
 }
-unrooted_qiime_default<-read.newick(file="input-files/tree.nwk")
+unrooted_qiime_default<-read.newick(file="catch-basin-microbes/input-files/tree.nwk")
 new.outgroup = pick_new_outgroup(unrooted_qiime_default)
 rootedTree = ape::root(unrooted_qiime_default, outgroup=new.outgroup, resolve.root=TRUE)
 newlyrootedTree<-read_tree(rootedTree, errorIfNULL=FALSE)
 
-ps.temp <- qza_to_phyloseq("input-files/table_analysis.qza", 
-                           "input-files/rooted_tree.qza", 
-                           "input-files/taxass_taxonomy_98.qza", 
-                           "input-files/metadata.txt")
+ps.temp <- qza_to_phyloseq("catch-basin-microbes/input-files/table_analysis.qza", 
+                           "catch-basin-microbes/input-files/rooted_tree.qza", 
+                           "catch-basin-microbes/input-files/taxass_taxonomy_98.qza", 
+                           "catch-basin-microbes/input-files/metadata.txt")
 
 # Create a new phyloseq object using a rerooted tree
 # this will be used for all downstream analyses
 
 ps.temp.rerooted <- phyloseq(otu_table(ps.temp), tax_table(ps.temp), sample_data(ps.temp), newlyrootedTree)
 ps.final <- subset_taxa(ps.temp.rerooted, Family != "Mitochondria")
-saveRDS(ps.final,"input-files/ps.final.rds")
+saveRDS(ps.final,"catch-basin-microbes/input-files/ps.final.rds")
 
 shannon <- diversity(t(otu_table(ps.final)), "shannon")
 asvfac = factor(rownames(tax_table(ps.final)))
